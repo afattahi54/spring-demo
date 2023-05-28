3924ae23bdb5e2e6f6b461260ea3c12e6bb60993
afattahi <afattahi@yahoo.com>
2023-05-28 10:33:12 +0330

    test release

--- src/main/java/com/example/spring/jpa/model/User.java
+++ src/main/java/com/example/spring/jpa/model/User.java
@@ -5,8 +5,6 @@ import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
